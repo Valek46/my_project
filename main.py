4138b650832f54a388b2bfc36e1aad3badd6ed74
Valek46 <valkushnir27@gmail.com>
2025-07-05 07:52:20 +0300

    middleware+depends+events_up_down_lifespan

--- main.py
+++ main.py
@@ -2,8 +2,41 @@ from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from routers import books
+from contextlib import asynccontextmanager
+from datetime import datetime
+import time
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Код при запуске
+    with open("state.txt", "a", encoding="utf-8") as f:
+        f.write(f"🚀 START at {datetime.now()}\n")
+
+    yield  # ⬅️ Переход к обработке запросов
+
+    # Код при завершении
+    with open("state.txt", "a", encoding="utf-8") as f:
+        f.write(f"🛑 SHUTDOWN at {datetime.now()}\n")
+
+app = FastAPI(lifespan=lifespan)
+
+@app.middleware("http")
+async def log_request(request: Request, call_next):
+    start_time = time.time()
+
+    client_ip = request.client.host
+    method = request.method
+    url = str(request.url)
+
+    print(f"[{method}] {url} — от {client_ip}")
+
+    response = await call_next(request)
+
+    duration = time.time() - start_time
+    print(f"Обработан за {duration:.3f} сек")
+
+    return response
+
 app.include_router(books.router)
 
 @app.exception_handler(RequestValidationError)
